cf4fb918f0c060bc598b5880837901d427c003dd
vincentole <63076449+vincentole@users.noreply.github.com>
2021-12-20 23:36:19 +0100

    added default btn type

--- user_error_modal/src/components/global/Button.jsx
+++ user_error_modal/src/components/global/Button.jsx
@@ -5,14 +5,14 @@ function Button(props, ref) {
     return (
         <button
             {...props}
-            type={props.type}
+            type={props.type || 'button'}
             className='bg-gray-700 text-white rounded py-1 px-3 focus:outline-2 focus:border-none focus:shadow-[0_0px_10px_2px_rgb(0,0,0,0.3)]'
             ref={ref}
         >
             {props.children}
         </button>
     );
-};
+}
 
 // eslint-disable-next-line no-func-assign
 Button = forwardRef(Button);
